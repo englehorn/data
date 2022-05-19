8e8775ea3a84b2cc1451f2f0ea4a9d3acf37371d
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-05-19 14:19:04 -0700

    Update TestData.js

--- TestData.js
+++ TestData.js
@@ -13,4 +13,22 @@
 		"MilePost": 14.47
 	},
 	"OpeningTime": "2022-04-25T11:45:00"
-}]
+},
+{
+	"Duration": 60,
+	"BridgeOpeningId": 50990,
+	"OriginalBridgeOpenScheduleDate": null,
+	"EventText": "Hood Canal Bridge closed, marine opening in progress. Motorists should expect delays for up to 60 minutes.",
+	"Status": "Closed to traffic ",
+	"BridgeLocation": {
+		"Description": "Hood Canal",
+		"Longitude": -122.6216,
+		"Latitude": 47.8584,
+		"Direction": "Both",
+		"RoadName": "104",
+		"MilePost": 14.47
+	},
+	"OpeningTime": "2022-04-25T11:45:00"
+},
+
+]
