cd22d73d5e52eb5a60c1d727de5c71d4bbd8da0b
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-05-20 09:36:07 -0700

    Update TestData.js

--- TestData.js
+++ TestData.js
@@ -12,13 +12,29 @@
 		"RoadName": "1",
 		"MilePost": 1
 	},
-	"OpeningTime": "2022-04-25T11:45:00"
+	"OpeningTime": "876876"
+},
+{
+	"Duration": 1,
+	"BridgeOpeningId": 1,
+	"OriginalBridgeOpenScheduleDate": null,
+	"EventText": "Hood Canal Bridge",
+	"Status": "1",
+	"BridgeLocation": {
+		"Description": "Hood Canal",
+		"Longitude": 1,
+		"Latitude": 1,
+		"Direction": "Both",
+		"RoadName": "1",
+		"MilePost": 1
+	},
+	"OpeningTime": "745745"
 },
 {
 	"Duration": 2,
-	"BridgeOpeningId": 2,
+	"BridgeOpeningId": 3,
 	"OriginalBridgeOpenScheduleDate": null,
-	"EventText": "Interstate Bridge",
+	"EventText": "Interstate Bridge3",
 	"Status": "2",
 	"BridgeLocation": {
 		"Description": "Interstate Bridge",
@@ -28,7 +44,7 @@
 		"RoadName": "2",
 		"MilePost": 2
 	},
-	"OpeningTime": "2022-04-25T11:45:00"
+	"OpeningTime": "4564564"
 },
 {
 	"Duration": 3,
@@ -44,7 +60,23 @@
 		"RoadName": "3",
 		"MilePost": 3
 	},
-	"OpeningTime": "2022-04-25T11:45:00"
+	"OpeningTime": "54645654645"
+},
+{
+	"Duration": 3,
+	"BridgeOpeningId": 4,
+	"OriginalBridgeOpenScheduleDate": null,
+	"EventText": "1st Avenue South Bridge in Seattlesss",
+	"Status": "3",
+	"BridgeLocation": {
+		"Description": "1st Avenue South Bridge in Seattle",
+		"Longitude": 3,
+		"Latitude": 3,
+		"Direction": "Both",
+		"RoadName": "3",
+		"MilePost": 3
+	},
+	"OpeningTime": "34234234"
 }
 
 ]
