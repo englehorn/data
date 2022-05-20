3eb3c457553a65047ef396778a36f5e274e1a7a9
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-05-20 11:54:54 -0700

    Update TestData.js

--- TestData.js
+++ TestData.js
@@ -1,3 +1,19 @@
 [
+{
+	"Duration": 1,
+	"BridgeOpeningId": 4,
+	"OriginalBridgeOpenScheduleDate": null,
+	"EventText": "Hood Canal Bridge",
+	"Status": "1",
+	"BridgeLocation": {
+		"Description": "Hood Canal",
+		"Longitude": 1,
+		"Latitude": 1,
+		"Direction": "Both",
+		"RoadName": "1",
+		"MilePost": 1
+	},
+	"OpeningTime": "745745"
+}
 
 ]
